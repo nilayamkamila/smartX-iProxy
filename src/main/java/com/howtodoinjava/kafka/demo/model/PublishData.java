c845696ebdb871b05dfb6e4af705199fd6c853a1
NILAYAM K KAMILA <nilayam_kamila@yahoo.com>
2022-06-18 20:13:51 -0400

    Initial Import

---
+++ src/main/java/com/howtodoinjava/kafka/demo/model/PublishData.java
@@ -0,0 +1,34 @@
+package com.howtodoinjava.kafka.demo.model;
+
+import java.util.Date;
+
+public class PublishData {
+    Date date;
+    String data;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    String message;
+}
